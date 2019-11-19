3beae6c9754176525d4aa40af442087828981f95
Bret Comnes <bcomnes@gmail.com>
2019-11-19 10:57:29 -0700

    init

---
+++ test.js
@@ -0,0 +1,18 @@
+import tape from 'tape';
+import ptape from 'tape-promise';
+import { getPagination } from './main';
+import fetch from 'node-fetch';
+const test = ptape(tape);
+
+test('header extraction', async t => {
+  fetch('https://api.github.com/users/bcomnes/repos')
+    .then(async response => {
+      const pagination = getPagination(response);
+      t.ok(pagination.next);
+      t.ok(pagination.last);
+      t.ok(pagination.current);
+      const json = await response.json();
+      return { pagination, json };
+    })
+    .catch(t.error);
+});
